4fc0a7a8a29d30d36132bc490aa8d60817b1cd35
Arseniy Zakharchenko <arseniyboss5@gmail.com>
2024-04-21 09:36:24 +0200

    replace vh with dvh for mobile devices

--- src/styles.tsx
+++ src/styles.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.main`
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  height: 100dvh;
   padding: 1rem;
   gap: 1rem;
 `
